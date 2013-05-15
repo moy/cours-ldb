1fb038eb474b409e79ca57a3e279e82e84b362ad
Matthieu Moy <Matthieu.Moy@imag.fr>
2013-05-15 09:39:16 +0200

    stat.c

---
+++ c/2-c-et-makefiles/stats.c
@@ -0,0 +1,40 @@
+/* 
+ * Implementation: stats.c
+ * Auteurs       : Enseignants du projet C Ensimag
+ * Implementation du module permettant de gérer des statistiques
+ * sur un ensemble de données fourni par l'utilisateur
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+/* Pour INT_MAX et INT_MIN */
+#include <limits.h>
+#include "stats.h"
+
+
+void stat_init()
+{
+	/* ... */
+}
+
+void stat_end()
+{
+	/* ... */
+}
+
+void stat_entrer_valeur(int value)
+{
+}
+
+
+int stat_min()
+{
+}
+
+int stat_max()
+{
+}
+
+float stat_moyenne()
+{
+}
